e9b783397c0dee57e475d31fcb598d9707cc3121
MSURES56 <MSURES56@WGC100718GMH2.chennai.ford.com>
2018-05-18 10:33:02 +0530

    Hiding transactions

--- festival/src/main/java/com/cinema/festival/FestivalApplication.java
+++ festival/src/main/java/com/cinema/festival/FestivalApplication.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class FestivalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FestivalApplication.class, args);
+	public static void main(String[] arg) {
+		SpringApplication.run(FestivalApplication.class, arg);
 	}
 }
